e2192dc1c508a25b2e38b9cfafd79008ac3b8861
TnovyLoo <loobrome@gmail.com>
2022-09-15 10:39:55 +0200

    initalizing new project

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# from . import views
+from .views import HomeView, ArticleDetailView, AddPostView
+
+urlpatterns = [
+    # path('', views.index, name='')
+    path('', HomeView.as_view(), name='index'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+
+]
---
+++ core/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
+from .forms import PostForm
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    # fields = '__all__' #we dont have to use it when we did a form
+    # # fields = ('title', 'body', 'title_tag')
