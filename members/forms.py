82c005666201ce114612d7e7dfdb7a26931263d4
TnovyLoo <loobrome@gmail.com>
2022-09-19 14:09:17 +0200

    Updating register form.

--- members/forms.py
+++ members/forms.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django import forms
 
 class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2',)
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2',)
 
     def __init__(self, *args, **kwargs):
         super(MyUserCreationForm, self).__init__(*args, **kwargs)
@@ -12,6 +17,10 @@ class MyUserCreationForm(UserCreationForm):
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['first_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+
 
 class MyUserAuthenticationForm(AuthenticationForm):
     class Meta:
