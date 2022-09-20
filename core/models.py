08e0c7dfd14d97bfd12104e543c698defd87e269
TnovyLoo <loobrome@gmail.com>
2022-09-20 13:07:57 +0200

    Creating profile page.

--- core/models.py
+++ core/models.py
@@ -16,7 +16,6 @@ class Profile(models.Model):
     social_link2 = models.CharField(max_length=255, null=True, blank=True)
     social_link3 = models.CharField(max_length=255, null=True, blank=True)
 
-
     def __str__(self):
         return str(self.user)
 
--- members/views.py
+++ members/views.py
@@ -3,6 +3,21 @@ from django.views import generic
 from django.contrib.auth.views import PasswordChangeView
 from .forms import MyUserCreationForm, MyUserAuthenticationForm, MyUserChangeForm, MyPasswordChangeForm
 from django.urls import reverse_lazy
+from django.views.generic import DetailView
+from core.models import Profile
+from django.shortcuts import get_object_or_404
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user-profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ShowProfilePageView, self).get_context_data(**kwargs)
+        profile_object = get_object_or_404(Profile, id=self.kwargs['pk'])
+
+        context['profile_object'] = profile_object
+
+        return context
 
 class PasswordsChangeView(PasswordChangeView):
     form_class = MyPasswordChangeForm
