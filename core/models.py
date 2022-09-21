797033d49ccb63bd124d59dc134856239f3a4bf4
TnovyLoo <loobrome@gmail.com>
2022-09-21 19:42:35 +0200

    Creating comment model.

--- core/models.py
+++ core/models.py
@@ -53,3 +53,12 @@ class Post(models.Model):
     def get_absolute_url(self):
         # return reverse('article-detail', args=(str(self.id)))
         return reverse('index')
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.post)
